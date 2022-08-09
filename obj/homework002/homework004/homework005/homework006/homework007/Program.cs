e626f08157fee9b28757a0b5d9854adae982e8f2
Vycheslav <general160473@yandex.ru>
2022-08-09 13:26:30 +0500

    add "fo homework"

---
+++ obj/homework002/homework004/homework005/homework006/homework007/Program.cs
@@ -0,0 +1,10 @@
+﻿// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++) 
+if (i % 2 == 0) 
+{
+    Console.WriteLine(i);
+}
